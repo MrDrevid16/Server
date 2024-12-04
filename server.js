cb09586f5cdac06215bdcb906b06e4e12f5c9de9
MrDrevid16 <davidelcacas16@gmail.com>
2024-12-04 04:24:03 -0600

    Backend preparado para deploy

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ config();
 // Configura la aplicación Express
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 // Configuración de multer para la subida de archivos
